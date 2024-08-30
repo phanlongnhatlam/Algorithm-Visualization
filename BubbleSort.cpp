bed259683d92a02aafdce9f549df220598ac1915
phanlongnhatlam <154681600+phanlongnhatlam@users.noreply.github.com>
2024-08-30 16:43:39 +0700

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,4 +1,4 @@
-﻿#include "graphics.h"
+#include "graphics.h"
 #include <windows.h>
 #include <iostream>
 #include <cstdlib>
@@ -12,11 +12,11 @@ void veMang(int a[], int n, int trai, int tren, int phai, int duoi) {
     char text[10];
     for (int i = 0; i < n; i++) {
         rectangle(trai, tren, phai, duoi);
-        sprintf_s(giatri, sizeof(giatri), "%d", a[i]);
+        sprintf_s(giatri, "%d", a[i]);
         double giatri_x = trai + (phai - trai) / 2 - 5;
         double giatri_y = tren + (duoi - tren) / 2.5;
         outtextxy(giatri_x, giatri_y, giatri);
-        sprintf_s(text, sizeof(text), "%d", i);
+        sprintf_s(text, "%d", i);
         int text_x = trai + (phai - trai) / 2 - 5;
         int text_y = duoi + 10;
         outtextxy(text_x, text_y, text);
@@ -24,10 +24,8 @@ void veMang(int a[], int n, int trai, int tren, int phai, int duoi) {
         phai += 100;
     }
 }
-
 // Hàm nhấp nháy hai hình chữ nhật cùng lúc
-void nhapNhayHaiHinhChuNhat(int trai1, int tren1, int phai1, int duoi1, int giatri1,
-    int trai2, int tren2, int phai2, int duoi2, int giatri2) {
+void nhapNhayHaiHinhChuNhat(int trai1, int tren1, int phai1, int duoi1, int giatri1,int trai2, int tren2, int phai2, int duoi2, int giatri2) {
     char giatri_txt1[10], giatri_txt2[10];
     sprintf_s(giatri_txt1, sizeof(giatri_txt1), "%d", giatri1);
     sprintf_s(giatri_txt2, sizeof(giatri_txt2), "%d", giatri2);
@@ -35,9 +33,10 @@ void nhapNhayHaiHinhChuNhat(int trai1, int tren1, int phai1, int duoi1, int giat
     double giatri_y1 = tren1 + (duoi1 - tren1) / 2.5;
     double giatri_x2 = trai2 + (phai2 - trai2) / 2 - 5;
     double giatri_y2 = tren2 + (duoi2 - tren2) / 2.5;
-
+    /* nhấp nháy */
     for (int k = 0; k < 3; k++) {
         setfillstyle(SOLID_FILL, YELLOW);
+
         bar(trai1, tren1, phai1, duoi1);
         outtextxy(giatri_x1, giatri_y1, giatri_txt1);
         rectangle(trai1, tren1, phai1, duoi1);
@@ -61,7 +60,7 @@ void nhapNhayHaiHinhChuNhat(int trai1, int tren1, int phai1, int duoi1, int giat
     }
 }
 
-// Hàm thực hiện thuật toán sắp xếp nổi bọt với minh họa
+// Hàm BubbleSort
 void sapXepNoiBot(int a[], int n, int luachon) {
     int trai = 50, tren = 100;
     int phai = 150, duoi = 150;
@@ -92,18 +91,15 @@ void sapXepNoiBot(int a[], int n, int luachon) {
 
             delay(500);
 
-            // So sánh và hoán đổi nếu cần
+            // So sánh và swap
             if ((luachon == 1 && a[j] > a[j + 1]) || (luachon == 2 && a[j] < a[j + 1])) {
-                nhapNhayHaiHinhChuNhat(trai_j, tren, phai_j, duoi, a[j],
-                    trai_j1, tren, phai_j1, duoi, a[j + 1]);
+                nhapNhayHaiHinhChuNhat(trai_j, tren, phai_j, duoi, a[j], trai_j1, tren, phai_j1, duoi, a[j + 1]);
 
-                int temp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = temp;
+                swap(a[j], a[j + 1]);
 
                 setfillstyle(SOLID_FILL, BLACK);
                 bar(trai_j, tren, phai_j, duoi);
-                sprintf_s(giatri, sizeof(giatri), "%d", a[j]);
+                sprintf_s(giatri,"%d", a[j]);
                 giatri_x = trai_j + (phai_j - trai_j) / 2 - 5;
                 giatri_y = tren + (duoi - tren) / 2.5;
                 outtextxy(giatri_x, giatri_y, giatri);
@@ -111,7 +107,7 @@ void sapXepNoiBot(int a[], int n, int luachon) {
 
                 setfillstyle(SOLID_FILL, BLACK);
                 bar(trai_j1, tren, phai_j1, duoi);
-                sprintf_s(giatri, sizeof(giatri), "%d", a[j + 1]);
+                sprintf_s(giatri, "%d", a[j + 1]);
                 giatri_x = trai_j1 + (phai_j1 - trai_j1) / 2 - 5;
                 giatri_y = tren + (duoi - tren) / 2.5;
                 outtextxy(giatri_x, giatri_y, giatri);
@@ -134,10 +130,8 @@ void sapXepNoiBot(int a[], int n, int luachon) {
                 outtextxy(giatri_x, giatri_y, giatri);
                 rectangle(trai_j1, tren, phai_j1, duoi);
             }
-
             delay(500);
         }
-
         // Làm nổi bật phần đã sắp xếp
         setfillstyle(SOLID_FILL, GREEN);
         int trai_da_sap_xep = 50 + (n - 1 - i) * 100;
@@ -149,8 +143,7 @@ void sapXepNoiBot(int a[], int n, int luachon) {
         outtextxy(giatri_x, giatri_y, giatri);
         rectangle(trai_da_sap_xep, tren, phai_da_sap_xep, duoi);
     }
-
-    // Làm nổi bật phần tử đầu tiên nếu đã sắp xếp
+    // Làm nổi bật phần tử đầu tiên sau khi sắp xếp xong
     setfillstyle(SOLID_FILL, GREEN);
     bar(trai, tren, phai, duoi);
     sprintf_s(giatri, sizeof(giatri), "%d", a[0]);
@@ -159,30 +152,25 @@ void sapXepNoiBot(int a[], int n, int luachon) {
     outtextxy(giatri_x, giatri_y, giatri);
     rectangle(trai, tren, phai, duoi);
 }
-
 int main() {
     int luachon;
     cout << "Ban muon sap xep tang dan hay giam dan?" << endl;
     cout << "1. Tang dan" << endl;
     cout << "2. Giam dan" << endl;
     cout << "Lua chon cua ban: "; cin >> luachon;
-
     initwindow(1000, 500, "Chương trình");
     settextstyle(BOLD_FONT, HORIZ_DIR, 2);
     char vanban[] = "Bubble Sort";
     outtextxy(375, 50, vanban);
-
     const int n = 8;
     int a[n] = {};
     srand(time(0));
     for (int i = 0; i < n; i++) {
         a[i] = rand() % 50;
     }
-
     veMang(a, n, 50, 100, 150, 150);
-
     sapXepNoiBot(a, n, luachon);
-
+    system("pause");
     getch();
     closegraph();
     return 0;
