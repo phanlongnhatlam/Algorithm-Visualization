d2528865b3f2e2d884fbecf5f527ef8798337a26
phanlongnhatlam <154681600+phanlongnhatlam@users.noreply.github.com>
2024-08-30 11:58:20 +0700

    Update TimKiemNhiPhan.cpp

--- TimKiemNhiPhan.cpp
+++ TimKiemNhiPhan.cpp
@@ -1,12 +1,10 @@
-﻿#include "graphics.h"
+#include "graphics.h"
 #include <windows.h>
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 #pragma comment(lib, "graphics.lib")
-
 using namespace std;
-
 // Hàm vẽ mảng
 void veMang(int a[], int n, int trai, int tren, int phai, int duoi) {
     char giatri[10];
@@ -25,7 +23,6 @@ void veMang(int a[], int n, int trai, int tren, int phai, int duoi) {
         phai += 100;
     }
 }
-
 // Hàm tô màu các phần tử đang so sánh
 void toMauDangSoSanh(int trai, int tren, int phai, int duoi, int giatri) {
     char giatri_txt[10];
@@ -37,7 +34,6 @@ void toMauDangSoSanh(int trai, int tren, int phai, int duoi, int giatri) {
     outtextxy(giatri_x, giatri_y, giatri_txt);
     rectangle(trai, tren, phai, duoi);
 }
-
 // Hàm tô màu các phần tử đã kiểm tra nhưng không khớp
 void toMauKhongKhop(int trai, int tren, int phai, int duoi, int giatri) {
     char giatri_txt[10];
@@ -49,13 +45,11 @@ void toMauKhongKhop(int trai, int tren, int phai, int duoi, int giatri) {
     outtextxy(giatri_x, giatri_y, giatri_txt);
     rectangle(trai, tren, phai, duoi);
 }
-
-// Hàm thực hiện thuật toán tìm kiếm nhị phân với minh họa
+// Hàm tìm kiếm nhị phân
 bool binarySearch(int a[], int n, int x) {
     int l = 0, r = n - 1;
     while (l <= r) {
         int m = (l + r) / 2;
-
         toMauDangSoSanh(50 + m * 100, 100, 150 + m * 100, 150, a[m]);
         delay(800);
 
@@ -68,38 +62,45 @@ bool binarySearch(int a[], int n, int x) {
             // Nếu không tìm thấy, tô màu phần tử không khớp
             toMauKhongKhop(50 + m * 100, 100, 150 + m * 100, 150, a[m]);
             delay(400);
-
             if (a[m] < x) {
                 l = m + 1;
-            } else {
+            }
+            else {
                 r = m - 1;
             }
         }
     }
     return false;
 }
-
+// Hàm sắp xếp nổi bọt
+void BubleSort(int* a, int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1]);
+            }
+        }
+    }
+}
 int main() {
-    initwindow(1000, 500, "Minh hoa Binary Search");
+    initwindow(1000, 500, "Binary Search");
     settextstyle(BOLD_FONT, HORIZ_DIR, 2);
-    char vanban[] = "Binary Search";
+    char vanban[] = "Tim kiem nhi phan";
     outtextxy(350, 50, vanban);
-
     const int n = 8;
-    int a[n] = {};  // Mảng đã sắp xếp
+    int a[n] = {};
     srand(time(0));
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         a[i] = rand() % 50;
     }
+    BubleSort(a, n);
     veMang(a, n, 50, 100, 150, 150);
-   
-    int x;  // Giá trị cần tìm
-    x = rand() % 50;
-
-    char message[50];  // Tạo một mảng ký tự đủ lớn để chứa thông điệp
+    int x;
+    cout << "Nhap gia tri can tim: ";
+    cin >> x;
+    char message[50];
     sprintf_s(message, sizeof(message), "Gia tri can tim la: %d", x);
-    outtextxy(350, 350, message);  // Hiển thị thông điệp lên màn hình
+    outtextxy(350, 350, message);
 
     if (binarySearch(a, n, x)) {
         sprintf_s(message, sizeof(message), "Tim thay gia tri %d!", x);
@@ -109,8 +110,7 @@ int main() {
         sprintf_s(message, sizeof(message), "Khong tim thay gia tri %d!", x);
         outtextxy(350, 400, message);
     }
-
-    getch();
+    system("pause");
     closegraph();
     return 0;
 }
