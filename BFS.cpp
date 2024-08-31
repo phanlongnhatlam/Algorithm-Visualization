fcccd396d5be01f50ffbe71e537b2088f52ce1c8
phanlongnhatlam <154681600+phanlongnhatlam@users.noreply.github.com>
2024-08-31 14:33:16 +0700

    Update BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -67,7 +67,7 @@ int y[MAX_NODES] = { 50, 150, 150, 250, 250, 250, 250, 350 };
 int charToIndex(char c) {
     return c - 'A';
 }
-// Hàm vẽ đỉnh với nhãn dễ nhìn hơn
+// Hàm vẽ đỉnh 
 void drawNode(int index, int borderColor, int fillColor) {
     setcolor(borderColor);
     circle(x[index], y[index], 20);
@@ -75,19 +75,20 @@ void drawNode(int index, int borderColor, int fillColor) {
     floodfill(x[index], y[index], borderColor);
     char label[2];
     sprintf_s(label, "%c", 'A' + index);
-    // Tăng kích thước chữ và căn chỉnh giữa
-    settextstyle(SANS_SERIF_FONT, HORIZ_DIR, 2);  // Kích thước chữ lớn hơn
+    settextstyle(SANS_SERIF_FONT, HORIZ_DIR, 2); 
     setbkcolor(fillColor);  // Đặt màu nền của chữ giống màu lấp đầy của node
     setcolor(BLACK);  // Màu chữ là màu đen để tương phản
-    outtextxy(x[index] - 10, y[index] - 10, label);  // Điều chỉnh vị trí nhãn để dễ nhìn hơn
+    outtextxy(x[index] - 10, y[index] - 10, label);  // Điều chỉnh vị trí nhãn 
 }
+// hàm vẽ các đường thẳng
 void drawEdge(int u, int v) {
     setcolor(WHITE);
     line(x[u], y[u], x[v], y[v]);
 }
+// hàm vẽ hàng đợi
 void drawQueue(ArrQueue& nodeQueue) {
     int queueX = 450;   // Tọa độ X bắt đầu của hàng đợi
-    int queueY = 350;   // Tọa độ Y bắt đầu của hàng đợi (từ dưới cùng)
+    int queueY = 350;   // Tọa độ Y bắt đầu của hàng đợi 
     int boxSize = 30;   // Kích thước mỗi ô trong hàng đợi
     // Xóa vùng hiển thị hàng đợi trước khi vẽ
     setfillstyle(SOLID_FILL, BLACK);  // Nền đen
@@ -116,16 +117,15 @@ void BFS(int startNode, int endNode) {
     visited[startNode] = true;
     while (!nodeQueue.isEmpty()) {
         int node = nodeQueue.DeQueue();
-        bfsResult[resultIndex++] = 'A' + node;
+        bfsResult[resultIndex++] = 'A' + node; // ví dụ A+3 thì ra D tại vì lưu vô mảng char <;
+        // nếu gặp tới node cần tới thì dừng
         if (node == endNode) {
             break;
         }
+        // đi từng node, node nào thăm rồi thì đánh dấu màu vàng, còn không thì vẫn là màu xám
         for (int i = 0; i < MAX_NODES; i++) {
             if (visited[i]) {
                 drawNode(i, YELLOW, YELLOW);
-            }
-            else {
-                drawNode(i, LIGHTGRAY, LIGHTGRAY);
             }
         }
         for (int i = 0; i < MAX_NODES; i++) {
